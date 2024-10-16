992c76a61a0ac51fe73b27c1cb2b16d7c6423e00
QMT <steamuhq@gmail.com>
2024-10-16 16:00:52 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,22 +1,5 @@
 # config for my projects
 
-from coloriz import Color
-import requests
-import os
 import customtkinter
-from PIL import Image, ImageTk
-from customtkinter import CTkImage  
-
-red = Color.RED
-green = Color.GREEN
-blue = Color.BLUE
-cyan = Color.CYAN
-magenta = Color.MAGENTA
-yellow = Color.YELLOW
-r = Color.RESET # RESEEEEEEEET
-
-def clear():
-    os.system('cls')
-
 
 app = customtkinter.CTk()
